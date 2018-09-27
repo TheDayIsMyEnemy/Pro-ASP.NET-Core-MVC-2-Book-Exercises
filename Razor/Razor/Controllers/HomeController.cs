a83fea4cf3ac664819bdd851865bb386cdc0c448
TheDayIsMyEnemy <34052076+TheDayIsMyEnemy@users.noreply.github.com>
2018-09-27 15:35:24 +0300

    razor exercise

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product[] array = {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 20.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+                };
+
+            return View(array);
+        }
+    }
+}
