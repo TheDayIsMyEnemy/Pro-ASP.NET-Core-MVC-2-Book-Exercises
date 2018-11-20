cbb403de184cc44fdaf30a5209ac88c681239a4f
Kristian Dimitrov <sickskills91@gmail.com>
2018-11-20 16:40:19 +0200

    configuring apps progress

---
+++ 01. Configuring Applications/ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using ConfiguringApps.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UptimeService uptime;
+
+        public HomeController(UptimeService up)
+        {
+            uptime = up;
+        }
+
+        public ViewResult Index()
+        {
+            return View(new Dictionary<string, string>
+            {
+                ["Message"] = "This is the index action",
+                ["Uptime"] = $"{uptime.Uptime}ms"
+            });
+        }
+    }
+}
---
+++ 01. Configuring Applications/ConfiguringApps/ConfiguringApps/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConfiguringApps.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConfiguringApps
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<UptimeService>();
+            services.AddMvc();  
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMiddleware<ShortCircuitMiddleware>();
+            app.UseMiddleware<ContentMiddleware>();
+        }
+    }
+}
