b1e7d308b947ddcf96b77e99577611a1bd3b1bfa
TheDayIsMyEnemy <sickskills91@gmail.com>
2019-01-20 21:43:01 +0200

    DI

---
+++ 04. Dependency Injection/DependencyInjection/DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using DependencyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRepository repository;
+        private ProductTotalizer totalizer;
+
+        public HomeController(IRepository repo, ProductTotalizer total)
+        {
+            repository = repo;
+            totalizer = total;
+        }
+
+        //public ViewResult Index()
+        //{
+        //    ViewBag.HomeController = repository.ToString();
+        //    ViewBag.Totalizer = totalizer.Repository.ToString();
+        //    return View(repository.Products);
+        //}
+            
+        // ACTION INJECTION
+        public ViewResult Index([FromServices]ProductTotalizer totalizer)
+        {
+            IRepository repository = 
+                (IRepository)HttpContext.RequestServices.GetService(typeof(IRepository)); // service locator pattern
+
+            ViewBag.Repository = repository.ToString();
+            ViewBag.RepositoryDirectlyFromServices = this.repository.ToString();
+            ViewBag.Totalizer = totalizer.Repository.ToString();
+            ViewBag.TotalizerFromActionInjection = this.totalizer.Repository.ToString();
+            return View(repository.Products);
+        }
+        
+    }
+}
---
+++ 04. Dependency Injection/DependencyInjection/DependencyInjection/Startup.cs
@@ -0,0 +1,31 @@
+﻿using DependencyInjection.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjection
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //TypeBroker.SetRepository<AlternateRepository>();
+
+           // services.AddSingleton<IRepository, MemoryRepository>();
+            //services.AddScoped<IRepository, MemoryRepository>(); 
+            services.AddTransient<IRepository, MemoryRepository>();
+            services.AddTransient<IModelStorage, DictionaryStorage>();
+            services.AddTransient<ProductTotalizer>();
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
