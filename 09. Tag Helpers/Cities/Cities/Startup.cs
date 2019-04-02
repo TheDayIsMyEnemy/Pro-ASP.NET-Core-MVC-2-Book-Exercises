87ea41abed4696e60faa27f3de52549bbf358f49
Kristian Dimitrov <sickskills91@gmail.com>
2019-04-02 16:40:14 +0300

    other built in tag helpers

--- 09. Tag Helpers/Cities/Cities/Startup.cs
+++ 09. Tag Helpers/Cities/Cities/Startup.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Cities.Models;
+﻿using Cities.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cities
